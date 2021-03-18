a34af78bb434cd5baa146e19a1b974bb13908466
Bryce Buchanan <Bryce.C.Buchanan-1@ou.edu>
2021-03-18 18:33:15 -0500

    Implemented Genetic Algorithm in 3D, and finished implementing main.py

--- GA_3D.py
+++ GA_3D.py
@@ -1,6 +1,6 @@
 """
 
-Implementation of Genetic Algorithm in 2D Search Space
+Implementation of Genetic Algorithm in 3D Search Space
 by: Bryce Buchanan
 
 """
@@ -11,10 +11,10 @@ import matplotlib.pyplot as plt
 """
 Initializes the chromosome using a uniform distribution between -5.0 and 5.0
 """
-def initialize2D():
+def initialize3D():
     chromosome = []
     for i in range(6):
-        gene = random.uniform(-5.0, 5.0)
+        gene = [random.uniform(-5.0, 5.0), random.uniform(-5.0, 5.0)]
         chromosome.append(gene)
     return chromosome
 
@@ -26,7 +26,7 @@ Returns: proportionate representation of the fitness scores
 def fitness(chromosome):
     fit_score = []
     for gene in chromosome:
-        fit = pow(gene, 4) - 22 * pow(gene, 2)
+        fit = pow(gene[0], 4) - 22 * pow(gene[0], 2) + pow(gene[1], 4) - 22 * pow(gene[1], 2)
         fit_score.append(fit)
     fit_sum = sum(fit_score)
     return fit_score
@@ -58,40 +58,32 @@ Implements crossover using the random number in range crossover operator
 
 Returns: the offspring of the two parents
 """
-def crossover2D(parent_one, parent_two):
-    offspring = []
+def crossover3D(parent_one, parent_two):
     if random.random() <= 0.5:
         #print("CROSSOVER OCCURED!")
-        if parent_one > parent_two:
-            temp = parent_one
-            parent_one = parent_two
-            parent_two = temp
-        offspring.append(random.uniform(parent_one, parent_two))
-        offspring.append(random.uniform(parent_one, parent_two))
-    else:
-        offspring.append(parent_one)
-        offspring.append(parent_two)
-    return offspring
+        temp = parent_one[0]
+        parent_one[0] = parent_two[0]
+        parent_two[0] = temp
+    return [parent_one, parent_two]
 
 """
 Implements mutation using the additive operator, with a 0.01% chance of mutation
 
 Returns: Either 0 (if no mutatation) or a random number between [-5.0, 5.0] (if mutation occuers)
 """
-def mutate2D(chromosome):
+def mutate3D(chromosome):
     if random.random() <= 0.01:
         #print("MUTATION OCCURED!")
-        return random.uniform(-5.0, 5.0)
+        return [random.uniform(-5.0, 5.0), random.uniform(-5.0, 5.0)]
     else:
-        return 0
+        return [0, 0]
 
 """
 Implements selection using the roulette wheel operator
 """
-def select2D(chromosome):
+def select3D(chromosome):
     selector = fitness(chromosome)
-    for element in selector:
-        selector[selector.index(element)] = element/sum(selector)
+    selector = [element/sum(selector) for element in selector]
     # Sorts the dictionary to compare values
     sort = sorted(selector)
     sort_dict = dict(zip(range(0,5),sort))
--- main.py
+++ main.py
@@ -9,30 +9,60 @@ ID: 113318419
 """
 from random import uniform
 
-from GA_2D import *
+from GA_2D import initialize2D, select2D, crossover2D, mutate2D
+from GA_3D import initialize3D, select3D, crossover3D, mutate3D
 from HC_2D import *
 from HC_3D import *
 
-#TODO Complete
 def GA_first(chromosome):
-    for i in range(1000):
-        # Selection
-        chromosome = select(chromosome)
-        # Crossover
-        cross = chromosome.copy()
-        random.shuffle(cross)
-        chromosome.clear()
-        for i in range(0, 3):
-            parent_one = cross.pop()
-            parent_two = cross.pop()
-            offspring = crossover(parent_one, parent_two)
-            chromosome.append(offspring[0])
-            chromosome.append(offspring[1])
-        # Mutation
-        for gene in chromosome:
-            gene += gene + mutate(chromosome)
-    return(chromosome)
+    avg_chromosome = []
+    for i in range(100):
+        for i in range(1000):
+            # Selection
+            chromosome = select2D(chromosome)
+            # Crossover
+            cross = chromosome.copy()
+            random.shuffle(cross)
+            chromosome.clear()
+            for i in range(0, 3):
+                parent_one = cross.pop()
+                parent_two = cross.pop()
+                offspring = crossover2D(parent_one, parent_two)
+                chromosome.append(offspring[0])
+                chromosome.append(offspring[1])
+            # Mutation
+            for gene in chromosome:
+                gene += mutate2D(chromosome)
+        avg_chromosome.append(sum(chromosome)/len(chromosome))
+    return sum(avg_chromosome)/len(avg_chromosome)
 
+
+def GA_second(chromosome):
+    avg_chromosome = []
+    for i in range(100):
+        for i in range(1000):
+            # Selection
+            chromosome = select3D(chromosome)
+            # Crossover
+            cross = chromosome.copy()
+            random.shuffle(cross)
+            chromosome.clear()
+            for i in range(0, 3):
+                parent_one = cross.pop()
+                parent_two = cross.pop()
+                offspring = crossover3D(parent_one, parent_two)
+                chromosome.append(offspring[0])
+                chromosome.append(offspring[1])
+            # Mutation
+            for gene in chromosome:
+                mutant = mutate3D(chromosome)
+                gene = [mutant[0], mutant[1]]
+        x = sum([element[0] for element in chromosome])/len(chromosome)
+        y = sum([element[1] for element in chromosome])/len(chromosome)
+        avg_chromosome.append([x, y])
+    avg_x = sum([element[0] for element in avg_chromosome])/len(avg_chromosome)
+    avg_y = sum([element[1] for element in avg_chromosome])/len(avg_chromosome)
+    return [avg_x, avg_y] 
 """
 Implements Hill Climbing in 2D API
 """
@@ -55,14 +85,14 @@ def HC_second():
 """
 Helper Function for plotting in 2D
 """
-def plot_2D(point):
+def plot_2D(point, title):
     fx = pow(np.linspace(-5,5,100), 4) - 22*pow(np.linspace(-5,5,100), 2)
     guess = pow(point, 4) - 22*pow(point, 2)
     plt.figure()
     plt.scatter(np.linspace(-5,5,100), fx, color='r', label='f(x)')
     plt.annotate('Max Estimate', (point, guess), xytext=(point + 1, guess + 1), 
                     arrowprops=dict(arrowstyle='->', connectionstyle='arc3'))
-    plt.title('Original Function')
+    plt.title(title)
     plt.ylabel('f(x)')
     plt.xlabel('x')
     plt.show()
@@ -70,30 +100,39 @@ def plot_2D(point):
 """
 Helper Function for plotting in 3D
 """
-def plot_3D(coords):
+def plot_3D(coords, title):
+    estimate = "Max Estimate: " + str(coords)
     dx, dy = np.meshgrid(np.linspace(-5,5,500), np.linspace(-5,5,500))
     fz = -22*dx**2 + dx**4 - 22*dy**2 + dy**4
     guess = -22*coords[0]**2 + coords[0]**4 - 22*coords[1]**2 + coords[1]**4
     plt.figure()
     ax = plt.axes(projection = '3d')
     ax.plot_surface(dx,dy,fz, cmap = 'viridis')
-    ax.set_title('Original Function')
+    ax.set_title(title)
     ax.set_zlabel("f(x,y)")
     ax.set_ylabel('y')
     ax.set_xlabel('x')
+    ax.text(-10, -10, 300, estimate)
     plt.show()
 
 def main():
-    # Chromosome initialization
-    chromosome = initialize()
-    chromosome = GA_first(chromosome)
-    print(chromosome)
+    # Chromosome initialization - 2D
+    chromosome2D = initialize2D()
+    chromosome2D = GA_first(chromosome2D)
+    
+    # Chromosome initialization - 3D
+    chromosome3D = initialize3D()
+    chromosome3D = GA_second(chromosome3D)
 
-    #hill_x = HC_first()
-    #hill_xy = HC_second()
+    plot_2D(chromosome2D, "Genetic Algorithm - 2D")
+    plot_3D(chromosome3D, "Genetic Algorithm - 3D")
 
-    #plot_2D(hill_x)
-    #plot_3D(hill_xy)
+    # Hill Climbing
+    hill_x = HC_first()
+    hill_xy = HC_second()
+
+    plot_2D(hill_x, 'Hill Climbing - 2D')
+    plot_3D(hill_xy, 'Hill Climbing - 3D')
     
 if __name__ == "__main__":
     main()
